a9b1086a728678ca2199e4190ef72d536fad5cac
Aarni Koskela <akx@iki.fi>
2019-09-13 09:57:32 +0300

    Update earth

--- earth.py
+++ earth.py
@@ -1,32 +1,20 @@
 from m5stack import *
 from m5ui import *
-from uiflow import *
 import unit
 
-setScreenColor(0x000000)
 earth0 = unit.get(unit.EARTH, unit.PORTA)
-
-
-ev = None
-
-
-def buttonA_wasPressed():
-    global ev
-    pass
-
-
-btnA.wasPressed(buttonA_wasPressed)
-
-
+dry_resistance_threshold = 0.5
 lcd.clear()
-lcd.setBrightness(0)
+lcd.setBrightness(200)
+lcd.font(lcd.FONT_Ubuntu)  # , 0, False)
 while True:
-    ev = earth0.analogValue
-    lcd.print(ev, 0, 0, 0xFFFFFF)
-    lcd.print((axp.getChargeI()), 0, 25, 0xFFCC66)
-    if ev < 25:
+    ev = earth0.analogValue / 1024
+    lcd.clear()
+    lcd.print("%d%%" % int(ev * 100), 10, 10, 0xFFFFFF)
+    if ev > 0 and ev < dry_resistance_threshold:
         M5Led.on()
+        speaker.tone(1600, 20)
+        wait_ms(60)
     else:
         M5Led.off()
     wait(1)
-    wait_ms(2)
